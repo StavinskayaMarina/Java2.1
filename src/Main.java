fbaf937983670cade5f7ef7bb6a32e7fd5f0c247
Stavinskaya_M <marina.st6277@gmail.com>
2023-04-13 19:45:26 +0600

    Bonus

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 19203; // сумма покупки
+        int bonus = 20; //количество рублей для одной бонусной мили
+        int sum = amount / bonus; // рассчет количества бонусных миль
+        System.out.println(sum);
+    }
+}
